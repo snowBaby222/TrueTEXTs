c6f13f2127c9602ee35791bad3e2c8cde7c3a101
AHMAD RAZA <96922532+snowBaby222@users.noreply.github.com>
2022-02-21 00:44:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
+
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 import React, { useState } from 'react';
-import{  //importing react router
-  BrowserRouter,
+/*import{  //importing react router
+  BroserRouter,
  // Switch,
   Route,
   Routes,
   //Link,
-} from 'react-router-dom';
+} from 'react-router-dom';*/
 
 function App() {
   const[mode, setMode] = useState('light')
@@ -19,46 +20,42 @@ function App() {
       setMode ('dark');
       document.body.style.backgroundColor = '#000033';
       document.body.style.color = 'white';
-      document.title = 'TrueTEXT - Dark Mode'
+      document.title = 'TrueTEXTs - Dark Mode'
+
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       document.body.style.color = 'black';
-      document.title = 'TrueTEXT - Light Mode'
+      document.title = 'TrueTEXTs - Light Mode'
+    }
+  }
+
+  const[mode2, setMode2] = useState('light')
+  const toggleMode2 = ()=>{
+    if(mode2 === 'light'){
+      setMode2 ('dark');
+      document.body.style.color = 'white';
+     
+
+    }
+    else{
+      setMode2('light');
+      document.body.style.color = 'black';
     }
   }
 
   return (   //returning JSX  -------------   Jsx will be compiled by babel.
    <>
-  { /*<BrowserRouter>
-<Navbar title='TrueTEXT' home='Home' about='About Us' mode={mode} toggleMode={toggleMode}/>
-<div className="container my-3">
-  
-<Routes>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path="/">
-    <TextForm heading='Analyze your text' textToAnalyze='Enter text to analyze:'/>
-    </Route>
-</Routes>
-</div>
-  </BrowserRouter>*/}
 
-<BrowserRouter>  {/*using react router*/}
 <Navbar title='TrueTEXTs' home='Home' about='About Us' mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
-    <Routes>
-      <Route exact path="/" element={<TextForm heading='Analyze your text' textToAnalyze='Enter text to analyze:'/>}>
-      </Route>
-      <Route exact path="About" element={<About />}>
-      </Route>
-    </Routes>
+    <TextForm heading='Analyze your text' mode={mode} toggleMode={toggleMode} textToAnalyze='Enter text to analyze:'/>
+    <Footer mode2={mode2} toggleMode2={toggleMode2}/>
 </div>
-  </BrowserRouter>,
 </>
-  );
+)
 }
+
 export default App;
 
