044c75eba4bfd51a93ad08826a73e7c5377be17c
AHMAD RAZA <96922532+snowBaby222@users.noreply.github.com>
2022-02-21 00:43:33 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
-/*import React from 'react'
+import React, {useState} from 'react'
 
-export default function Footer() {
+export default function Footer(props) {
+  const [text] = useState("© Copyright 2022 - TrueTEXTs | All rights reserved.");
+  const [text2] = useState("Developed by AHMAD");
   return (
-    <div className='container'>
-     <p>Developed by Ahmad</p>
-    </div>
+    <>
+<div class="text-center">
+    <p class={`${props.mode2==='light'?'dark':'light'}`}>{text}</p>
+    <p class={`${props.mode2==='light'?'dark':'light'}`}>{text2}</p>
+    
+</div>
+
+</>
   )
 }
-*/
